1ab771bfd7e128162f543d2ea41c6e0d07a25d52
Lowxorx <lowxorx@lahan.fr>
2020-09-24 14:38:10 +0200

    Add http interceptor to prevent caching

--- SwtorOptimizer/ClientApp/src/app/app.module.ts
+++ SwtorOptimizer/ClientApp/src/app/app.module.ts
@@ -1,22 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { NavMenuModule } from './components/widgets/nav-menu/nav-menu.module';
+import { NoCacheHeadersInterceptor } from './http-interceptor';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [
-    AppRoutingModule,
-    BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
-    HttpClientModule,
-    BrowserAnimationsModule,
-    NavMenuModule
+  imports: [AppRoutingModule, BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }), HttpClientModule, BrowserAnimationsModule, NavMenuModule],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: NoCacheHeadersInterceptor,
+      multi: true,
+    },
   ],
-  providers: [],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
