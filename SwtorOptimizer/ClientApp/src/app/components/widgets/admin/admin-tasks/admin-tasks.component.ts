a1f90a6df3c501266d74ef3e503bb8bfcb3cf023
Lowxorx <lowxorx@lahan.fr>
2020-09-29 15:26:13 +0200

    Add task auto refresh on admin

--- SwtorOptimizer/ClientApp/src/app/components/widgets/admin/admin-tasks/admin-tasks.component.ts
+++ SwtorOptimizer/ClientApp/src/app/components/widgets/admin/admin-tasks/admin-tasks.component.ts
@@ -1,13 +1,12 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, NgZone, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
-import { Subscription } from 'rxjs';
+import { interval, Subscription } from 'rxjs';
 import { CalculationTaskStatus } from 'src/app/enums/CalculationTaskStatus';
 import CalculationTaskHelper from 'src/app/helpers/calculation-task.helper';
 import { ICalculationTask } from 'src/app/models/ICalculationTask';
-import { AccountService } from 'src/app/services/account.service';
 import { AdminService } from 'src/app/services/admin.service';
 import { CalculationTasksService } from 'src/app/services/calculation-tasks.service';
 
@@ -16,20 +15,36 @@ import { CalculationTasksService } from 'src/app/services/calculation-tasks.serv
   templateUrl: './admin-tasks.component.html',
   styleUrls: ['./admin-tasks.component.scss'],
 })
-export class AdminTasksComponent implements OnInit {
+export class AdminTasksComponent implements OnInit, OnDestroy {
   public displayedColumns: string[] = ['threshold', 'status', 'setsFound', 'action'];
   public dataSource: MatTableDataSource<ICalculationTask> = new MatTableDataSource();
+  public tasksNumber = 0;
   private dataSourceSubscription: Subscription;
+  private timerSub: Subscription;
 
   @ViewChild(MatSort, { static: true })
   public sort: MatSort;
   @ViewChild(MatPaginator)
   public paginator: MatPaginator;
 
-  constructor(private accountService: AccountService, private snackBar: MatSnackBar, private adminService: AdminService, private taskService: CalculationTasksService) {}
+  constructor(private snackBar: MatSnackBar, private adminService: AdminService, private taskService: CalculationTasksService, private ngZone: NgZone) {}
 
   public ngOnInit(): void {
     this.refreshTasks();
+    this.watchTasks();
+  }
+
+  public ngOnDestroy(): void {
+    try {
+      if (this.dataSourceSubscription) {
+        this.dataSourceSubscription.unsubscribe();
+      }
+      if (this.timerSub) {
+        this.timerSub.unsubscribe();
+      }
+    } catch (error) {
+      // Sub is already unsubscribed
+    }
   }
 
   public getTaskStatus(task: ICalculationTask): string {
@@ -64,9 +79,18 @@ export class AdminTasksComponent implements OnInit {
     return taskStatus === CalculationTaskStatus.Started;
   }
 
+  private watchTasks(): void {
+    this.timerSub = interval(10000).subscribe(() => {
+      this.ngZone.run(() => {
+        this.refreshTasks();
+      });
+    });
+  }
+
   private refreshTasks(): void {
     this.dataSourceSubscription = this.taskService.getAllTasks().subscribe({
       next: (tasks) => {
+        this.tasksNumber = tasks.length;
         this.dataSource = new MatTableDataSource(tasks);
         this.dataSource.sort = this.sort;
         this.dataSource.paginator = this.paginator;
